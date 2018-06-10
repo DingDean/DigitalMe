3e5de8b9485e6cd8932c6c957ad6ec8a745416ea
DingDean <dingkework@hotmail.com>
2018-06-10 09:48:43 +0800

    main: use grpc service Pager for ping event and split out database
    grpc provides a faster approach than zeromq for a single developer.
    Currently, there are two services based on grpc. A pager service, which
     would be embeded into the main http server which open to web interface.
     The other is a database service which runs independently, both the
     editor client and http server would be connected to it.

--- main.js
+++ main.js
@@ -4,12 +4,8 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const path = require('path')
-const debug = require('debug')('digitme')
-const relay = require('./src/relay.js')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/digitalme')
-const mongo = require('./src/mongodb')
-const utils = require('./src/utils.js')
+const debug = require('debug')('dgmc')
+const pager = require(path.resolve(__dirname, './services/pager'))
 
 let staticPath = path.resolve(__dirname, './pwa-digitalme/dist')
 app.use('/', express.static(staticPath))
@@ -25,47 +21,14 @@ io.on('connection', socket => {
     debug('A user disconnected')
   })
 
-  socket.on('greeting', (msg) => {
-    debug(`Client says ${msg}`)
-    socket.emit('greeting', 'yes')
+  socket.on('ping', (msg) => {
+    debug(`ping`)
+    socket.emit('pong', 'hello there')
   })
 })
 
+pager.run('0.0.0.0:50052', null, io)
+
 http.listen(8765, () => {
   debug('listening on 8765')
-})
-
-relay.on('digit_ping', data => {
-  debug('ping')
-  io.sockets.emit('char')
-})
-
-relay.on('digit_session', data => {
-  debug('Session received:')
-  debug(data)
-  let {history = [], tomatos = []} = data
-
-  let reportInfo = utils.parseForReport(history)
-  for (let qstring in reportInfo) {
-    if (!reportInfo.hasOwnProperty(qstring)) continue
-    let { date, hour, langs } = reportInfo[qstring]
-    mongo.updateReport({date, hour}, langs, (err, doc) => {
-      if (err) {
-        debug('Failed to update report')
-        debug(err)
-      }
-    })
-  }
-
-  let langInfo = utils.parseForLangs(history)
-  mongo.updateLangs(langInfo)
-
-  let tomatoInfo = utils.parseForTomatos(tomatos)
-  mongo.updateTomatos(tomatoInfo)
-})
-
-let RELAY_PORT = process.env.RELAY_PORT || 8764
-relay.listen(`tcp://*:${RELAY_PORT}`, err => {
-  if (err) return debug(err)
-  debug(`Relay server listening on ${RELAY_PORT}...`)
 })
